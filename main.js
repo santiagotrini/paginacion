6df1f7d0e170141e0a0a4407a968a94904706a06
Santiago Trini <santiagotrini@gmail.com>
2024-08-07 20:52:14 -0300

    agrega input para filtrar

--- main.js
+++ main.js
@@ -1,44 +1,67 @@
-let page = 1;
-let url = 'https://rickandmortyapi.com/api/character?page='
 
-function handlePrev(e) {
-  if (page > 1)  page--;
-  if (page == 1) e.currentTarget.disabled = true;
-  if (page < 42) e.currentTarget.nextElementSibling.disabled = false;
-  updateList();
+let filterInput = document.querySelector('input');
+let btnPrev = document.querySelector('#prev');
+let btnNext = document.querySelector('#next');
+btnPrev.addEventListener('click', handlePrev);
+btnNext.addEventListener('click', handleNext);
+filterInput.addEventListener('input', handleInput);
+filterInput.value = '';
+
+
+let currentPage = 1;
+let currentName = null;
+let lastPage = 42;
+
+function handlePrev() {
+  if (currentPage > 1) currentPage--;
+  if (currentPage < lastPage) btnNext.disabled = false;
+  if (currentPage == 1) btnPrev.disabled = true;
+  fetchCharacters(currentPage, currentName);
 }
 
-function handleNext(e) {
-  if (page < 42)  page++;
-  if (page == 42) e.currentTarget.disabled = true;
-  if (page > 1)   e.currentTarget.previousElementSibling.disabled = false;
-  updateList();
+function handleNext() {
+  if (currentPage < lastPage) currentPage++;
+  if (currentPage > 1) btnPrev.disabled = false;
+  if (currentPage == lastPage) btnNext.disabled = true;
+  fetchCharacters(currentPage, currentName);
 }
 
-function updateList() {
-  fetch(url + page)
-  .then(res => res.json())
-  .then(data => {
-    let container = document.querySelector('.container');
-    container.innerHTML = '';
-    for (let c of data.results) {
-      let div = document.createElement('div');
-      let img = document.createElement('img');
-      let h3  = document.createElement('h2');
-      div.classList.add('card');
-      img.src = c.image;
-      h3.textContent = c.name;
-      div.append(img);
-      div.append(h3);
-      container.append(div);
-    }
-    document.querySelector('#page').textContent = `${page}/42`;
-  });
+function fetchCharacters(page, name) {
+  let url = 'https://rickandmortyapi.com/api/character';
+  url += `?page=${page ? page : 1}`;
+  if (name) url += `&name=${name}`;
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      lastPage = data.info.pages;
+      document.querySelector('#page').textContent = `${page}/${lastPage}`;
+      let container = document.querySelector('.container');
+      container.innerHTML = '';
+      for (let character of data.results) {
+        let card = document.createElement('div');
+        let title = document.createElement('h1');
+        let img = document.createElement('img');
+        card.classList.add('card');
+        title.textContent = character.name;
+        img.src = character.image;
+        card.append(img);
+        card.append(title);
+        container.append(card);
+      }
+    });
 }
 
+function handleInput(e) {
+  if (e.target.value.length >= 1 && e.target.value.length <= 3) return;
+  if (e.target.value == '') {
+    currentPage = 1;
+    currentName = null;
+  } else {
+    currentName = e.target.value;
+    currentPage = 1;
+  }
+  fetchCharacters(currentPage, currentName);
+}
 
-document.querySelector('#prev').addEventListener('click', handlePrev);
-document.querySelector('#next').addEventListener('click', handleNext);
-
-
-updateList();
+fetchCharacters(currentPage, currentName);
